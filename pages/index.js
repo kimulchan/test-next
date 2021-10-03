94f4b98e4878e5df1f08496f207eaa866bfe6115
kimulchan <leerabi577@gmail.com>
2021-10-03 23:27:00 +0900

    Img 추가

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
+import Layout from '../components/layout';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <Link href="posts/pages">
-      <a>this page</a>
-    </Link>
+    <Layout>
+    <Head>
+      <title>의찬잉</title>
+      <link rel="icon" href="/favicon.ico"></link>
+    </Head>
+      <Link href="posts/pages">
+        <a>this page</a>
+      </Link>
+      <Image src="/images/profile.jpg" height={144} width={144} alt="kimulcahn"></Image>
+    </Layout>
   )
 }

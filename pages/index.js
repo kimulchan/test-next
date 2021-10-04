439ba21d269bac570f54688b9e1e0580d6bf869e
kimulchan <leerabi577@gmail.com>
2021-10-04 22:17:33 +0900

    다음 next 공부를 위한 세팅

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link';
-import Layout from '../components/layout';
-import styles from '../styles/Home.module.css'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <Layout>
-    <Head>
-      <title>의찬잉</title>
-      <link rel="icon" href="/favicon.ico"></link>
-    </Head>
-      <Link href="posts/pages">
-        <a>this page</a>
-      </Link>
-      <Image src="/images/profile.jpg" height={144} width={144} alt="kimulcahn"></Image>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
     </Layout>
   )
 }
--- pages/posts/pages.js
+++ pages/posts/pages.js
@@ -1,8 +1,15 @@
+import Head from "next/head";
 import Link from "next/link";
 
 function pages (){
+
     return (
+    <>
+        <Head>
+            <title>First Post</title>
+        </Head>
         <Link href="/"><a>backPage</a></Link>
+    </>
     );
 }
 export default pages;
